37ce049c0d66adb44a858b8ea00cc19d6b835ff1
kennykded <82447341+kennykded@users.noreply.github.com>
2022-07-19 21:44:33 +0100

    Sorting Algorithms

--- sort.h
+++ sort.h
@@ -30,16 +30,6 @@ void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void shell_sort(int *array, size_t size);
-void cocktail_sort_list(listint_t **list);
-void counting_sort(int *array, size_t size);
-void merge_sort(int *array, size_t size);
-void heap_sort(int *array, size_t size);
-void radix_sort(int *array, size_t size);
-void bitonic_sort(int *array, size_t size);
 void quick_sort_hoare(int *array, size_t size);
-void sort_deck(deck_node_t **deck);
-void swap_list(listint_t *pn, listint_t *temp, listint_t *nxt, listint_t **h);
-void swap(int *arr, int idx1, int idx2);
-
 
 #endif
